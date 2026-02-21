a532cb9488b71f56143545ccc5cb5f0b7ba80eed
SebastianRamos01 <sebasr.sr@hotmail.com>
2026-02-21 18:09:35 -0300

    data and links added

--- src/data/dataLinks.ts
+++ src/data/dataLinks.ts
@@ -1,6 +1,6 @@
 const links = [
-    { id: 1, title: 'Website', href: 'https://example.com' },
-    { id: 2, title: 'Phone', href: 'https://twitter.com/tuusuario' },
-    { id: 3, title: 'Email', href: 'mailto:tu@correo.com' }
+    { id: 1, title: 'Website', href: 'https://portfolio-redesign-alpha.vercel.app/'},
+    { id: 2, title: 'Phone', href: 'https://www.instagram.com/dsign.sba/' },
+    { id: 3, title: 'Email', href: 'mailto:sebasm.sr@gmail.com' }
 ];
 export default links;
