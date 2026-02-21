f89210d1369c19b2b47d8d1b7924e0efeed8f270
SebastianRamos01 <sebasr.sr@hotmail.com>
2026-02-21 20:12:49 -0300

    details

--- src/data/dataLinks.ts
+++ src/data/dataLinks.ts
@@ -1,6 +1,6 @@
 const links = [
-    { id: 1, title: 'Website', href: 'https://portfolio-redesign-alpha.vercel.app/'},
-    { id: 2, title: 'Phone', href: 'https://www.instagram.com/dsign.sba/' },
+    { id: 1, title: 'Instagram', href: 'https://www.instagram.com/dsign.sba/'},
+    { id: 2, title: 'Phone', href: 'tel:+011 3783-8175'},
     { id: 3, title: 'Email', href: 'mailto:sebasm.sr@gmail.com' }
 ];
 export default links;
